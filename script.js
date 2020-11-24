6a02621c2ade8f1085687ad70fde007e76ef76ef
Nikola <nikola.tomasovic@medieinstitutet.se>
2020-11-24 11:53:18 +0100

    copied in my files from another folder

---
+++ script.js
@@ -0,0 +1,11 @@
+// Börja med att skapa en skola som ett objekt. Objektet ska existera innuti en variabel som ni namnger med skolans namn för att göra det simpel. Skolan ska innehålla egenskaperna: name, address, zipcode, city, students med värdet av en tom array och teachers som en tom array. Till exempel:
+
+let medieinstitutet = {
+    name: "medieinstitutet",
+    address: [],
+    zipcode: [],
+    city: []
+    students: [],
+    teachers: [],
+    
+};
